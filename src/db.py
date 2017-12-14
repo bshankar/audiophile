9da1e04a1cdc808f88ab1718cb622878bab46cbd
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-12-14 13:08:15 +0530

    convert to str before setting key value pairs redis

--- src/db.py
+++ src/db.py
@@ -4,16 +4,16 @@ r = redis.StrictRedis()
 
 def store(addresses, times, song_hash, song_name):
     for i in range(len(addresses)):
-        r.set('note:' + addresses[i], (song_hash << 13) + times[i])
+        r.set('note:' + str(addresses[i]), (song_hash << 13) + times[i])
     r.set('song:%d' % song_hash, song_name)
 
 
 def search(addresses):
     results = {}
     for add in addresses:
-        info = r.get('note:' + add)
+        info = r.get('note:' + str(add))
         if info is not None:
-            song_name = r.get('song:' + (info >> 13))
+            song_name = r.get('song:' + str(int(info) >> 13))
             if song_name in results:
                 results[song_name] += 1
             else:
